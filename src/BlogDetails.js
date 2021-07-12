5cb393197345b1d48dcc46ac9cf6be0a633dc2c5
unknown <kinchoily@hotmail.com>
2021-07-12 13:25:36 +0100

    Reusing useFetch Custom Hook to gather and fill in data for BlogDetails page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom"
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data: blog, error, isLoading } = useFetch('http://localhost:8000/blogs/' + id); // (url End point for useFetch to gather its data)
 
     return (
         <div className="blog-details">
-            <h2>Blog Details - {id} </h2>
+            {isLoading && <div>Loading...</div>} {/* && conditionally output template dependent on these values*/}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written By {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
 
     )
