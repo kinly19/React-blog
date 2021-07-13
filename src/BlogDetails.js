208d3b41812db82b5c003bcaafc01fe6f39b817d
unknown <kinchoily@hotmail.com>
2021-07-13 12:50:28 +0100

    created delete button to handle delete request

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,27 @@
-import { useParams } from "react-router-dom"
+import { useHistory, useParams } from "react-router-dom"
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isLoading } = useFetch('http://localhost:8000/blogs/' + id); // (url End point for useFetch to gather its data)
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
+
+    // const handleEdit = () => {
+    //     fetch('http://localhost:8000/blogs/' + blog.id, {
+    //         method: 'GET'
+    //     }).then(() => {
+    //         history.push('/edit')
+    //     })
+    // }
+
 
     return (
         <div className="blog-details">
@@ -14,6 +32,8 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written By {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleClick}>Delete</button>
+                    {/* <button onClick={handleEdit}>Edit</button> */}
                 </article>
             )}
         </div>
